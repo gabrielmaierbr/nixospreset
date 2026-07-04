973949fb8bfc2488b97b0d29ab3fb7c1ece17723
Gabriel Maier <116469635+gabrielmaierbr@users.noreply.github.com>
2026-07-04 13:14:53 -0300

    Delete hypr directory

--- hypr/configs/execs.lua
+++
@@ -1,4 +0,0 @@
-hl.on("hyprland.start", function () 
-    hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
-    hl.exec_cmd("noctalia")
-end)
--- hypr/configs/monitor.lua
+++
@@ -1,9 +0,0 @@
-hl.monitor({
-    output   = "",
-    mode     = "2560x1440@180",
-    position = "0x0",
-    scale    = 1,
-    bitdepth = 10,
-    cm = "dcip3",
-    supports_hdr = -1,
-})
