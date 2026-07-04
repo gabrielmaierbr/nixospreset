674ea9ec3f3c8719f338235342af3e89bd7cd095
Gabriel Maier <116469635+gabrielmaierbr@users.noreply.github.com>
2026-07-04 09:58:12 -0300

    Configure Steam and GameMode in services.nix
    Added Steam and GameMode configurations with firewall settings and additional packages.

--- modules/services.nix
+++ modules/services.nix
@@ -3,4 +3,26 @@
 	services.power-profiles-daemon.enable = true;
     services.upower.enable = true;
     services.printing.enable = true;
+
+	programs.steam = {
+	 	enable = true;
+		remotePlay.openFirewall = true;
+    	dedicatedServer.openFirewall = true;
+	};
+
+	programs.gamemode.enable = true;
+	programs.steam.package = pkgs.steam.override {
+	  extraPkgs = pkgs': with pkgs'; [
+	    libXcursor
+	    libXi
+	    libXinerama
+	    libXScrnSaver
+	    libpng
+	    libpulseaudio
+	    libvorbis
+	    stdenv.cc.cc.lib
+	    libkrb5
+	    keyutils
+	  ];
+	};
 }
